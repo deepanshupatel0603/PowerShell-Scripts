f452b95f6c7b8904540d1d46d94c48cd93bdb9ea
deepanshupatel0603 <48382030+deepanshupatel0603@users.noreply.github.com>
2025-11-15 02:35:57 +0530

    Add files via upload

---
+++ SetServiceRecoveryMode.ps1
@@ -0,0 +1,14 @@
+﻿#Recovery Mode of Service
+function Set-ServiceRecovery {
+    param (
+        [string] [Parameter(Mandatory=$true)] $ServiceName,
+        [int] $resetPeriod = 86400, # in seconds (1 day)
+        [int] $restartDelay = 60000 # in milliseconds (1 minute)
+    )
+
+    $actions = "restart/$restartDelay/restart/$restartDelay/restart/$restartDelay"
+    sc.exe failure $ServiceName reset= $resetPeriod actions= $actions
+}
+
+# Example usage
+Set-ServiceRecovery -ServiceName "YourServiceName"
