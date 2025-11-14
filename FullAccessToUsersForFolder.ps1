841f64d2ab7d1489ab4992867ffa53955f8ca18e
deepanshupatel0603 <48382030+deepanshupatel0603@users.noreply.github.com>
2025-11-15 02:15:08 +0530

    Add files via upload
    This script has code to give full permissions to users.

---
+++ FullAccessToUsersForFolder.ps1
@@ -0,0 +1,20 @@
+﻿#Give Users Full Control/Grant Full Control
+ 
+$Folder = ($envProgramFilesX86 + "\CostWork")
+icacls.exe $folder /grant 'USERS:(OI)(CI)(F)'
+
+#OR
+
+$dir= "${env:ProgramFiles(x86)}\Template"
+icacls.exe $dir /grant 'USERS:(OI)(CI)(F)'
+
+#OR
+
+if(Test-Path -Path "$env:ProgramFiles\Autodesk\MDM")
+{
+    $Acl = Get-Acl "$env:ProgramFiles\Autodesk\MDM"
+    $Permission = "everyone", "FullControl", "ContainerInherit,ObjectInherit", "None", "Allow"
+    $rule = New-Object System.Security.AccessControl.FileSystemAccessRule $permission
+    $Acl.SetAccessRule($rule)
+    $Acl | Set-Acl "$env:ProgramFiles\Autodesk\MDM"
+}
