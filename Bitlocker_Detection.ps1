839f9d9d341fa96ea1ea674f9259f1381f6be454
deepanshupatel0603 <48382030+deepanshupatel0603@users.noreply.github.com>
2026-01-16 11:36:52 -0500

    Add files via upload

---
+++ Bitlocker_Detection.ps1
@@ -0,0 +1,56 @@
+﻿<#
+AUTHOR: Deepanshu Patel (deepanshupatel0603)
+.SYNOPSIS
+This script can be used to Detect Bitlocker encryption is 128 bit for Laptop only.
+.DESCRIPTION
+This script can be used to Detect Bitlocker encryption is 128 bit for Laptop only.
+.EXAMPLE
+C:\PS> C:\Script\BitlockerDetection.ps1
+To Detect Bitlocker encryption is 128 bit.
+#>
+
+#Function to create log file
+
+function Write-Log
+{
+   Param ([string]$LogString)
+
+   $LogFile = "C:\Windows\Temp\BitlockerStateDetection.log"
+
+   $DateTime = "[{0:MM/dd/yy} {0:HH:mm:ss}]" -f (Get-Date)
+
+   $LogMessage = "$Datetime $LogString"
+
+   Add-content $LogFile -value $LogMessage
+}
+
+#checking if device is Laptop or Desktop
+$hardwareType = (Get-WmiObject -Class Win32_ComputerSystem -Property PCSystemType).PCSystemType
+
+if(($hardwareType -ne $null) -and ($hardwareType -eq 2))
+{
+    Write-Log "******** Device has PCSystemType = `"$hardwareType`" ********"
+    
+    $encrypMethod = (Get-BitLockerVolume -MountPoint C:).EncryptionMethod
+
+    Write-Log "******** Encryption Method is: `"$encrypMethod`" ********"
+
+    $bStatus = (Get-CimInstance -ClassName Win32_Battery).BatteryStatus
+    Write-Log "******** Battery Status is: `"$bStatus`" ********"
+
+    if(($encrypMethod -Like '*128') -and ($bStatus -eq 2))
+    {
+        Write-Log "******** Encryption Method is: `"$encrypMethod`" and Power cable is connected so exiting for remediation script to run ********"
+        Exit 1
+    }
+    Else
+    {
+        Write-Log "******** Either Encryption Method is `"256 bit`" or Power cable is not connected. ********"
+        Exit 0
+    }
+}
+Else
+{
+    Write-Log "******** Device is not Laptop, so skipping this action ********"
+    Exit 0
+}
---
+++ Bitlocker_Remediation.ps1
@@ -0,0 +1,68 @@
+﻿<#
+AUTHOR: Deepanshu Patel (deepanshupatel0603)
+.SYNOPSIS
+This script can be used to Remediate Bitlocker to 256 bit encryption.
+.DESCRIPTION
+This script can be used to Remediate Bitlocker to 256 bit encryption.
+.EXAMPLE
+C:\PS> C:\Script\Bitlocker_Remediation.ps1
+To Remediate Bitlocker to 256 bit encryption.
+#>
+
+#Create log file using PS1
+
+function Write-Log
+{
+   Param ([string]$LogString)
+
+   $LogFile = "C:\Windows\Temp\BitlockerStateRemediation.log"
+
+   $DateTime = "[{0:MM/dd/yy} {0:HH:mm:ss}]" -f (Get-Date)
+
+   $LogMessage = "$Datetime $LogString"
+
+   Add-content $LogFile -value $LogMessage
+}
+
+Write-Log "******** Decryption started, as detection script found that Encryption State is 128 bit ********"
+
+#Start-Process -FilePath "powershell.exe" -ArgumentList "Suspend-BitLocker -MountPoint `"C:`""
+Suspend-BitLocker -MountPoint C: -ErrorAction SilentlyContinue
+Start-Sleep -Seconds 60
+
+#Start-Process -FilePath "powershell.exe" -ArgumentList "Disable-BitLocker -MountPoint `"C:`""
+Disable-BitLocker -MountPoint C: -ErrorAction SilentlyContinue
+Start-Sleep -Seconds 120
+
+do{
+    $VolumeStatus = (Get-BitLockerVolume -MountPoint C:).VolumeStatus
+    $encrypStatus = (Get-BitLockerVolume -MountPoint C:).EncryptionPercentage
+    $protStatus = (Get-BitLockerVolume -MountPoint C:).ProtectionStatus
+
+}until(($VolumeStatus -eq 'FullyDecrypted') -and ($encrypStatus -eq 0) -and ($protStatus -eq 'Off'))
+
+Write-Log "******** Decryption done, now changing Encryption Method to 256 bit ********"
+
+#Enable-BitLocker -MountPoint C: -EncryptionMethod XtsAes256
+
+do{
+    $VolumeStatus = (Get-BitLockerVolume -MountPoint C:).VolumeStatus
+    $encrypStatus = (Get-BitLockerVolume -MountPoint C:).EncryptionPercentage
+    $protStatus = (Get-BitLockerVolume -MountPoint C:).ProtectionStatus
+    $encrypMethod = (Get-BitLockerVolume -MountPoint C:).EncryptionMethod
+
+    if(($VolumeStatus -ne $null) -and ($encrypStatus -eq $null))
+    {
+        if(($VolumeStatus -eq 'FullyEncrypted') -and ($encrypStatus -eq 100))
+        {
+            Write-Log "******** Resuming Bitlocker ********"
+            Resume-BitLocker -MountPoint C:
+            Start-Sleep -Seconds 90
+        }
+    }
+
+}until(($encrypMethod -Like '*256') -and ($VolumeStatus -eq 'FullyEncrypted') -and ($encrypStatus -eq 100) -and ($protStatus -eq 'On'))
+
+Exit 0
+
+Write-Log "******** BitlockerState Remediation script ended... ********"
