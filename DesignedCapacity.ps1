e6f23f36366f2c10f083f27fcbb70532e3d6c0e4
deepanshupatel0603 <48382030+deepanshupatel0603@users.noreply.github.com>
2026-01-16 11:56:29 -0500

    Add files via upload

---
+++ DesignedCapacity.ps1
@@ -0,0 +1,27 @@
+﻿<#
+AUTHOR: Deepanshu Patel (deepanshupatel0603)
+
+This script will check DesignedCapacity and add it in registry.
+#>
+
+$computer = $env:COMPUTERNAME
+$namespace = "ROOT\WMI"
+$classname = "BatteryStaticData"
+
+<# 
+Write-Output "====================================="
+Write-Output "COMPUTER : $computer "
+Write-Output "CLASS    : $classname "
+Write-Output "====================================="
+#>
+
+$DesignDCap = (Get-WmiObject -Class $classname -ComputerName $computer -Namespace $namespace | Select-Object DesignedCapacity).DesignedCapacity #-ExcludeProperty PSComputerName, Scope, Path, Options, ClassPath, Properties, SystemProperties, Qualifiers, Site, Container | Format-List -Property [a-z]*
+
+if(!(Test-Path -Path "HKLM:\Software\DesignCapacity"))
+{
+    New-Item -Path "HKLM:\Software\DesignCapacity" -ItemType Directory -Force
+}
+
+New-ItemProperty -Path "HKLM:\Software\DesignCapacity" -Name 'DesignedCapacity' -Value $DesignDCap -PropertyType String -ErrorAction SilentlyContinue
+
+#Get-WmiObject -Class $classname -ComputerName $computer -Namespace $namespace | Select-Object * -ExcludeProperty PSComputerName, Scope, Path, Options, ClassPath, Properties, SystemProperties, Qualifiers, Site, Container | Format-List -Property [a-z]*
---
+++ HideFolderfor1809-1607.ps1
@@ -0,0 +1,33 @@
+﻿<#
+AUTHOR: Deepanshu Patel (deepanshupatel0603)
+
+This script will hide folders for Windows 1809-1607. This also has unhide command but it is commented.
+#>
+
+#Log function to create log
+function WriteLog{  
+  
+Param ([string]$LogString)    
+$LogFile = "C:\Windows\Temp\REGN_HideFolderfor1809-1607_v1.0.log"    
+$DateTime = "[{0:MM/dd/yy} {0:HH:mm:ss}]" -f (Get-Date)    
+$LogMessage = "$Datetime $LogString"    
+Add-content $LogFile -value $LogMessage
+}
+
+#Hiding folder for DEV Environment
+WriteLog "**************** Checking for DEV Folder ****************"
+
+$FolDEV="$env:ProgramData\Microsoft\Windows\Start Menu\Programs\AnyFolder"
+if($FolDEV -ne $null)
+{
+    if(Test-Path -Path $FolDEV)
+    {
+        $F_DEV=Get-Item "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\AnyFolder" -Force -ErrorAction SilentlyContinue
+        WriteLog "**************** DEV Folder Found, so hiding it ****************"
+        $F_DEV.Attributes = 'Hidden'
+        WriteLog "**************** DEV Folder hidden successfully ****************"
+    }
+}
+
+#Unhide folder
+#$Folder.Attributes = $Folder.Attributes -bxor [System.IO.FileAttributes]::Hidden
